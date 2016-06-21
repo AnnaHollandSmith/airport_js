79591aac25e7faaa517544561475917344ba5d03
Anna <anna.hollandsmith3@gmail.com>
2016-06-21 17:43:24 +0100

    added functionalty for preventing landing in storms

--- spec/features/FeatureSpec.js
+++ spec/features/FeatureSpec.js
@@ -13,7 +13,6 @@ describe('Feature Test:', function(){
   To get passengers to a destination
   I want to instruct a plan to land at
   an airport and confirm that it has landed*/
-
   it('planes can be instructed to land at an airport', function(){
     plane.land(airport);
     expect(airport.planes()).toContain(plane);
@@ -23,7 +22,6 @@ describe('Feature Test:', function(){
   To get passengers to a destination
   I want to instruct a plan to take off from
   an airport and confirm that it is no longer in the airport*/
-
   it('can instruct a plane to take off', function(){
     plane.land(airport);
     plane.takeoff();
@@ -34,10 +32,18 @@ describe('Feature Test:', function(){
   To ensure safety
   I want to prevent takeoff when weather is stormy*/
   it('blocks takeoff when weather is stormy', function(){
-  plane.land(airport)
-  spyOn(airport,'isStormy').and.returnValue(true);
-  expect(function(){ plane.takeoff();}).toThrowError('cannot takeoff during storm');
-  expect(airport.planes()).toContain(plane);
-});
+    plane.land(airport)
+    spyOn(airport,'isStormy').and.returnValue(true);
+    expect(function(){ plane.takeoff();}).toThrowError('cannot takeoff during storm');
+    expect(airport.planes()).toContain(plane);
+  });
 
+  /*As an air traffic controller
+  To ensure safety
+  I want to prevent takeoff when weather is stormy*/
+  it('blocks takeoff when weather is stormy', function(){
+    spyOn(airport,'isStormy').and.returnValue(true);
+    expect(function(){ plane.land(airport);}).toThrowError('cannot land during storm');
+    expect(airport.planes()).not.toContain(plane);
+  });
 });
