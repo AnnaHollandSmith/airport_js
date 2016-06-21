4b8ad0d7e95f780020898b02287744c7a79478e7
Anna <anna.hollandsmith3@gmail.com>
2016-06-21 15:40:43 +0100

    added functionality for not taking off in stormy weather

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -24,4 +24,14 @@ describe('Airport', function(){
     expect(airport.planes()).toEqual([]);
   });
 
+  it('can check for stormy weather', function(){
+    expect(airport.isStormy()).toBeFalsy();
+  });
+
+  describe('under stormy conditions',function(){
+    it('does not clear planes for takeoff', function(){
+      spyOn(airport,'isStormy').and.returnValue(true);
+      expect(function(){ airport.clearForTakeOff(plane); }).toThrowError('cannot takeoff during storm');
+    });
+  });
 });
